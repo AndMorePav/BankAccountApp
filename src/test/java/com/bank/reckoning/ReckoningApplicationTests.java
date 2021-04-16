b7cb8520cc2c08159cef6c5d7cdadaa484237f8e
Andrey Morozov <morozovandrej4758@gmail.com>
2021-04-16 15:49:05 +0300

    Main commit

---
+++ src/test/java/com/bank/reckoning/ReckoningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.reckoning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReckoningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
