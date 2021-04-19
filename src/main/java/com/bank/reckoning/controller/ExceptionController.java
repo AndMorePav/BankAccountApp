d3c5d6b80ec920539c5b4a9a81316a7482a198a6
Andrey Morozov <morozovandrej4758@gmail.com>
2021-04-19 11:32:55 +0300

    Docks fix

--- src/main/java/com/bank/reckoning/controller/ExceptionController.java
+++ src/main/java/com/bank/reckoning/controller/ExceptionController.java
@@ -6,11 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+/**
+ * Exceptions handler.
+ */
 @ControllerAdvice
 public class ExceptionController {
 
+    /**
+     * Method for creating new user account.
+     *
+     * @param repeatPasswordNotSameException for work with exception
+     * @return response object
+     */
     @ExceptionHandler(value = RepeatPasswordNotSameException.class)
-    public ResponseEntity<Object> exception(RepeatPasswordNotSameException exception) {
-        return new ResponseEntity<>("RepeatPassword not same", HttpStatus.BAD_REQUEST);
+    public ResponseEntity<Object> exception(RepeatPasswordNotSameException repeatPasswordNotSameException) {
+        return new ResponseEntity<>(repeatPasswordNotSameException.getCause(), HttpStatus.BAD_REQUEST);
     }
 }
