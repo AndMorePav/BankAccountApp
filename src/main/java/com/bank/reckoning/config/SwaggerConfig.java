01c008ac8796c264ea42cf672972783b476b9c6b
Andrey Morozov <morozovandrej4758@gmail.com>
2021-05-21 16:58:28 +0300

    Fix SwaggerConfig

--- src/main/java/com/bank/reckoning/config/SwaggerConfig.java
+++ src/main/java/com/bank/reckoning/config/SwaggerConfig.java
@@ -1,25 +1,67 @@
 package com.bank.reckoning.config;
 
+import com.bank.reckoning.config.properties.SwaggerConfigProperties;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Swagger configuration.
  */
 @Configuration
 @EnableSwagger2
+@EnableConfigurationProperties(SwaggerConfigProperties.class)
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final SwaggerConfigProperties swaggerConfigProperties;
+
     @Bean
     public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.bank.reckoning.controller"))
+//                .paths(PathSelectors.ant("/**"))
+//                .build();
+
         return new Docket(DocumentationType.SWAGGER_2)
+                .host(swaggerConfigProperties.getHost())
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.bank.reckoning.controller"))
-                .paths(PathSelectors.ant("/**"))
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.regex("/*.*"))
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        SecurityReference securityReference = new SecurityReference("JWT", new AuthorizationScope[]{
+                new AuthorizationScope("global", "accessEverything")
+        });
+
+        return Collections.singletonList(securityReference);
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
     }
 }
